76333e169cb4a0b15088912e75b00473b4200de9
aminsarshar <sarsharamin85@gmail.com>
2025-10-11 18:26:41 +0330

    create componnet livewire

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -24,58 +24,8 @@
 
                             <h4 class="header-title m-t-0 m-b-30">نمونه اول</h4>
 
-                            <div class="table-responsive">
-                                <table id="mainTable" class="table table-striped m-b-0">
-                                    <thead>
-                                        <tr>
-                                            <th>نام</th>
-                                            <th>هزینه</th>
-                                            <th>سود</th>
-                                            <th>تفریح</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>ماشین</td>
-                                            <td>100</td>
-                                            <td>200</td>
-                                            <td>0</td>
-                                        </tr>
-                                        <tr>
-                                            <td>دوچرخه</td>
-                                            <td>330</td>
-                                            <td>240</td>
-                                            <td>1</td>
-                                        </tr>
-                                        <tr>
-                                            <td>بیلیارد</td>
-                                            <td>430</td>
-                                            <td>540</td>
-                                            <td>3</td>
-                                        </tr>
-                                        <tr>
-                                            <td>قایق بادبانی</td>
-                                            <td>100</td>
-                                            <td>200</td>
-                                            <td>0</td>
-                                        </tr>
-                                        <tr>
-                                            <td>اسکی</td>
-                                            <td>330</td>
-                                            <td>240</td>
-                                            <td>1</td>
-                                        </tr>
-                                    </tbody>
-                                    <tfoot>
-                                        <tr>
-                                            <th><strong>مجموع</strong></th>
-                                            <th></th>
-                                            <th></th>
-                                            <th></th>
-                                        </tr>
-                                    </tfoot>
-                                </table>
-                            </div>
+                            <livewire:admin.users.index />
+
                         </div>
                     </div><!-- end col -->
                 </div>
